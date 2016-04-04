575ebcf44d9895938baa3e120cf172ad1d68a2e5
Maciej Szarlinski <mszarlinski@bravurasolutions.com>
2016-04-04 10:07:01 +0200

    Initial Angular app generated with Yoeman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularCustomComponentsTalkApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularCustomComponentsTalkApp
+ */
+angular.module('angularCustomComponentsTalkApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularCustomComponentsTalkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularCustomComponentsTalkApp
+ */
+angular.module('angularCustomComponentsTalkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
